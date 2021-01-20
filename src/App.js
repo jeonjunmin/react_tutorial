3fed9e630230b26e3327a836033d77a2a78e88fe
jeonjunmin <azeros314@naver.com>
2021-01-20 22:11:21 +0900

    component

--- src/App.js
+++ src/App.js
@@ -14,11 +14,39 @@ class Subject extends Component {
   }
 }
 
+
+class Toc extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h1>HTML</h1>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Subject></Subject>
+        <Toc></Toc>
+        <Content></Content>
       </div>
     );    
   }
