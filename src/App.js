06b9f101021fd5edcebe3c93a2703ca2039560c3
jeonjunmin <azeros314@naver.com>
2021-04-08 23:46:55 +0900

    React - 20.3

--- src/App.js
+++ src/App.js
@@ -70,29 +70,28 @@ class App extends Component {
           {id:this.max_content_id, title:_title, desc:_desc}
         )
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         })
                
       }.bind(this)}></CreateContent>
     }else if (this.state.mode === 'update'){
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title,_desc){        
-        this.max_content_id = this.max_content_id+1;
-        
-        //**push로 데이터 삽입시 기존데이터를 바꾼다.
-        // this.state.contents.push(
-        //   {id:this.max_content_id, title:_title, desc:_desc}
-        // )
-        // this.setState({
-        //   contents:this.state.contents
-        // })
-        
-        //**concat로 데이터 삽입시 기존데이터를 건드리지 않는다.
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        )
+      _article = <UpdateContent data={_content} onSubmit={function(_id, _title,_desc){        
+        var _contents = Array.from(this.state.contents); //this.state.contents의 값을 복사한 새로운 배열을 만든다.
+        var i = 0;
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id, title:_title, desc:_desc};        
+            break;
+          }
+          i = i+1;
+        }
+
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:'read'
         })
        
         
--- src/components/UpdateCONTENT.js
+++ src/components/UpdateCONTENT.js
@@ -5,6 +5,7 @@ class UpdateContent extends Component {
     constructor(props){
       super(props);
       this.state = {
+        id:this.props.data.id,
         title:this.props.data.title,
         desc:this.props.data.desc
       }
@@ -23,9 +24,10 @@ class UpdateContent extends Component {
           <h2>Update</h2>
          <form action="/create_process" method="post" onSubmit={function(e){
            e.preventDefault();           
-           this.props.onSubmit(e.target.title.value,e.target.desc.value);
+           this.props.onSubmit(this.state.id, this.state.title, this.state.desc);
            
          }.bind(this)}>
+           <input type="hidden" name="id" value={this.state.id}></input>
            <p><input type="text" name ="title" placeholder="title" value={this.state.title} onChange={this.inputFormHandler}></input></p>
            <p>
              <textarea name="desc" placeholder="description" value={this.state.desc} onChange={this.inputFormHandler}></textarea>
